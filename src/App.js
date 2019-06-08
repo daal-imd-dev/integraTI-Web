72a32cbb0891dba7d4d731d49f3edf0d98e33a7d
gellyviana <gellyvianamota1988@gmail.com>
2019-06-07 22:01:34 -0300

    Adicionado css e aplicado conceito de modularização do React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
+import Home from './Home'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>IntegraTI</h1>
-        <span> Sua plataforma de auxílio aos seus estudos.</span>
-      </div>
-    )
+    <div className="App">
+      <Home/>
+    </div>);
   }
 }
 
