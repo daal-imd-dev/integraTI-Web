8909d52b0f2965fc060df19668ee3a1fde171da5
gellyviana <gellyvianamota1988@gmail.com>
2019-06-16 18:27:08 -0300

    Criado Cards e rotas do Integra-TI

---
+++ src/Atualizese.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Cards from './cards';
+
+
+
+class Atualizese extends Component{
+    render(){
+        return(
+            <div className="Menu">
+                <Cards/>
+            </div>
+            
+           
+        );
+    }
+
+}
+
+export default Atualizese
---
+++ src/Main.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Switch, Route } from 'react-router-dom';
+import { Container } from 'reactstrap';
+import Home from './Home';
+import Sejavisto from './Seja_visto';
+import Atualizese from './Atualizese'
+import Aboutus from './About_us'
+import Faqs from './Faqs'
+
+class Main extends React.Component{
+    render(){
+        return(
+            <main>
+                <Container>
+                    <Switch>
+                    <Route exact path='/home' component={Home}/>
+                    <Route path='/sejavisto' component={Sejavisto}/>
+                    <Route path='/atualizese' component={Atualizese}/>
+                    <Route path='/aboutus' component={Aboutus}/>
+                    <Route path='/faqs' component={Faqs}/>
+                    </Switch>
+                </Container>
+            </main> 
+
+        );
+    }
+}
+     
+
+
+export default Main;
