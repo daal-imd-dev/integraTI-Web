f03be6c4406fd697f2981f642b911394a6b3ad54
assis_25 <assis25@gmail.com>
2019-06-18 10:50:12 -0300

    Página About

--- src/About_us.js
+++ src/About_us.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
-
+import {Card, Col, Row, Container, CardTitle, CardBody} from 'reactstrap';
 class About_us extends Component{
     render(){
         return(
-            <div className="Container">
-        
-            </div>
+            <Container>
+                <Row>
+                    <Col m={8} s={12}>
+                        <h5 className="subtitle">About Us</h5>
+                        <Card>
+                            <CardBody>
+                                <CardTitle>
+                                    <h5 className="title">Diretório Acadêmico Ada Lovelace - Tecnologia da Informação - UFRN</h5>
+                                </CardTitle>
+                            <div>
+                                <p>
+                                    Entidade de representação dos estudantes do BTI - UFRN com suas áreas de concentração 
+                                    em Engenharia de Software, Ciência da Computação, Sistemas de Informação de Gestão, 
+                                    Informática Educacional, Sistemas Embarcados e Bioinformática.
+                                </p>
+                            </div>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 
